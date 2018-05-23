fb61ea8e4480b6bc39fe412949d05c51c7cb0c4a
[A[A[A[B[B[Bwilltebbutt <wt0881@my.bristol.ac.uk>
2018-05-24 00:23:33 +0100

    Partial implementation of composite distribution

--- src/Turing.jl
+++ src/Turing.jl
@@ -8,145 +8,150 @@ module Turing
 #       to indicate that's not implemented inside Turing.jl            #
 ########################################################################
 
-using Distributions
-using ForwardDiff
-import ReverseDiff
-using ProgressMeter
-using Stan
-using ReverseDiff: GradientTape, GradientConfig, gradient!, compile, TrackedArray
+using Distributions, MacroTools
 
-import Base: ~, convert, promote_rule, rand, getindex, setindex!
-import Distributions: sample
-import ForwardDiff: gradient
-import ReverseDiff: gradient
-import Mamba: AbstractChains, Chains
-import Stan: Adapt, Hmc
+import Distributions: logpdf
 
-##############################
-# Global variables/constants #
-##############################
+include("core/composite_distribution.jl")
 
-global ADBACKEND = :reverse_diff
-setadbackend(backend_sym) = begin
-  @assert backend_sym == :forward_diff || backend_sym == :reverse_diff
-  global ADBACKEND = backend_sym
-end
+# using ForwardDiff
+# import ReverseDiff
+# using ProgressMeter
+# using Stan
+# using ReverseDiff: GradientTape, GradientConfig, gradient!, compile, TrackedArray
 
-global ADSAFE = false
-setadsafe(switch::Bool) = begin
-  println("[Turing]: global ADSAFE is set as $switch")
-  global ADSAFE = switch
-end
+# import Base: ~, convert, promote_rule, rand, getindex, setindex!
+# import Distributions: sample
+# import ForwardDiff: gradient
+# import ReverseDiff: gradient
+# import Mamba: AbstractChains, Chains
+# import Stan: Adapt, Hmc
 
-global CHUNKSIZE = 0        # default chunksize used by AD
-global SEEDS                # pre-alloced dual parts
-setchunksize(chunk_size::Int) = begin
-  if ~(CHUNKSIZE == chunk_size)
-    println("[Turing]: AD chunk size is set as $chunk_size")
-    global CHUNKSIZE = chunk_size
-    global SEEDS = ForwardDiff.construct_seeds(ForwardDiff.Partials{chunk_size,Float64})
-  end
-end
+# ##############################
+# # Global variables/constants #
+# ##############################
 
-setchunksize(40)
+# global ADBACKEND = :reverse_diff
+# setadbackend(backend_sym) = begin
+#   @assert backend_sym == :forward_diff || backend_sym == :reverse_diff
+#   global ADBACKEND = backend_sym
+# end
 
-global PROGRESS = true
-turnprogress(switch::Bool) = begin
-  println("[Turing]: global PROGRESS is set as $switch")
-  global PROGRESS = switch
-end
+# global ADSAFE = false
+# setadsafe(switch::Bool) = begin
+#   println("[Turing]: global ADSAFE is set as $switch")
+#   global ADSAFE = switch
+# end
 
-global VERBOSITY = 1        # verbosity for dprintln & dwarn
-global const FCOMPILER = 0  # verbose printing flag for compiler
+# global CHUNKSIZE = 0        # default chunksize used by AD
+# global SEEDS                # pre-alloced dual parts
+# setchunksize(chunk_size::Int) = begin
+#   if ~(CHUNKSIZE == chunk_size)
+#     println("[Turing]: AD chunk size is set as $chunk_size")
+#     global CHUNKSIZE = chunk_size
+#     global SEEDS = ForwardDiff.construct_seeds(ForwardDiff.Partials{chunk_size,Float64})
+#   end
+# end
 
-# Constans for caching
-global const CACHERESET  = 0b00
-global const CACHEIDCS   = 0b10
-global const CACHERANGES = 0b01
+# setchunksize(40)
 
-global TRANS_CACHE = Dict{Tuple,Any}()
+# global PROGRESS = true
+# turnprogress(switch::Bool) = begin
+#   println("[Turing]: global PROGRESS is set as $switch")
+#   global PROGRESS = switch
+# end
 
-#######################
-# Sampler abstraction #
-#######################
+# global VERBOSITY = 1        # verbosity for dprintln & dwarn
+# global const FCOMPILER = 0  # verbose printing flag for compiler
 
-abstract type InferenceAlgorithm end
-abstract type Hamiltonian <: InferenceAlgorithm end
+# # Constans for caching
+# global const CACHERESET  = 0b00
+# global const CACHEIDCS   = 0b10
+# global const CACHERANGES = 0b01
 
-doc"""
-    Sampler{T}
+# global TRANS_CACHE = Dict{Tuple,Any}()
 
-Generic interface for implementing inference algorithms.
-An implementation of an algorithm should include the following:
+# #######################
+# # Sampler abstraction #
+# #######################
 
-1. A type specifying the algorithm and its parameters, derived from InferenceAlgorithm
-2. A method of `sample` function that produces results of inference, which is where actual inference happens.
+# abstract type InferenceAlgorithm end
+# abstract type Hamiltonian <: InferenceAlgorithm end
 
-Turing translates models to chunks that call the modelling functions at specified points. The dispatch is based on the value of a `sampler` variable. To include a new inference algorithm implements the requirements mentioned above in a separate file,
-then include that file at the end of this one.
-"""
-type Sampler{T<:InferenceAlgorithm}
-  alg   ::  T
-  info  ::  Dict{Symbol, Any}         # sampler infomation
-end
+# doc"""
+#     Sampler{T}
 
-include("helper.jl")
-include("transform.jl")
-include("core/varinfo.jl")  # core internal variable container
-include("trace/trace.jl")   # to run probabilistic programs as tasks
+# Generic interface for implementing inference algorithms.
+# An implementation of an algorithm should include the following:
 
-using Turing.Traces
-using Turing.VarReplay
+# 1. A type specifying the algorithm and its parameters, derived from InferenceAlgorithm
+# 2. A method of `sample` function that produces results of inference, which is where actual inference happens.
 
-###########
-# Exports #
-###########
+# Turing translates models to chunks that call the modelling functions at specified points. The dispatch is based on the value of a `sampler` variable. To include a new inference algorithm implements the requirements mentioned above in a separate file,
+# then include that file at the end of this one.
+# """
+# type Sampler{T<:InferenceAlgorithm}
+#   alg   ::  T
+#   info  ::  Dict{Symbol, Any}         # sampler infomation
+# end
 
-# Turing essentials - modelling macros and inference algorithms
-export @model, @~, @VarName                   # modelling
-export MH, Gibbs                              # classic sampling
-export HMC, SGLD, SGHMC, HMCDA, NUTS          # Hamiltonian-like sampling
-export IS, SMC, CSMC, PG, PIMH, PMMH, IPMCMC  # particle-based sampling
-export sample, setchunksize, resume           # inference
-export auto_tune_chunk_size!, setadbackend, setadsafe # helper
-export dprintln, set_verbosity, turnprogress  # debugging
+# include("helper.jl")
+# include("transform.jl")
+# include("core/varinfo.jl")  # core internal variable container
+# include("trace/trace.jl")   # to run probabilistic programs as tasks
 
-# Turing-safe data structures and associated functions
-export TArray, tzeros, localcopy, IArray
+# using Turing.Traces
+# using Turing.VarReplay
 
-export @sym_str
+# ###########
+# # Exports #
+# ###########
 
-export UnivariateGMM2, Flat, FlatPos
+# # Turing essentials - modelling macros and inference algorithms
+# export @model, @~, @VarName                   # modelling
+# export MH, Gibbs                              # classic sampling
+# export HMC, SGLD, SGHMC, HMCDA, NUTS          # Hamiltonian-like sampling
+# export IS, SMC, CSMC, PG, PIMH, PMMH, IPMCMC  # particle-based sampling
+# export sample, setchunksize, resume           # inference
+# export auto_tune_chunk_size!, setadbackend, setadsafe # helper
+# export dprintln, set_verbosity, turnprogress  # debugging
 
-##################
-# Turing helpers #
-##################
+# # Turing-safe data structures and associated functions
+# export TArray, tzeros, localcopy, IArray
 
-set_verbosity(v::Int) = global VERBOSITY = v
+# export @sym_str
 
-doc"""
-    dprintln(v, args...)
+# export UnivariateGMM2, Flat, FlatPos
 
-Debugging print function. The first argument controls the verbosity of message.
-"""
-dprintln(v::Int, args...) = v < Turing.VERBOSITY ?
-                            println("\r[Turing]: ", args..., "\n $(stacktrace()[2])") :
-                            nothing
-dwarn(v::Int, args...)    = v < Turing.VERBOSITY ?
-                            print_with_color(:red, "\r[Turing.WARNING]: ", mapreduce(string,*,args), "\n $(stacktrace()[2])\n") :
-                            nothing
-derror(v::Int, args...)   = error("\r[Turing.ERROR]: ", mapreduce(string,*,args))
+# ##################
+# # Turing helpers #
+# ##################
 
-##################
-# Inference code #
-##################
+# set_verbosity(v::Int) = global VERBOSITY = v
 
-include("depreciated.jl")
-include("core/util.jl")         # utility functions
-include("core/compiler.jl")     # compiler
-include("core/container.jl")    # particle container
-include("core/io.jl")           # I/O
-include("samplers/sampler.jl")  # samplers
-include("core/ad.jl")           # Automatic Differentiation
+# doc"""
+#     dprintln(v, args...)
+
+# Debugging print function. The first argument controls the verbosity of message.
+# """
+# dprintln(v::Int, args...) = v < Turing.VERBOSITY ?
+#                             println("\r[Turing]: ", args..., "\n $(stacktrace()[2])") :
+#                             nothing
+# dwarn(v::Int, args...)    = v < Turing.VERBOSITY ?
+#                             print_with_color(:red, "\r[Turing.WARNING]: ", mapreduce(string,*,args), "\n $(stacktrace()[2])\n") :
+#                             nothing
+# derror(v::Int, args...)   = error("\r[Turing.ERROR]: ", mapreduce(string,*,args))
+
+# ##################
+# # Inference code #
+# ##################
+
+# include("depreciated.jl")
+# include("core/util.jl")         # utility functions
+# include("core/compiler.jl")     # compiler
+# include("core/container.jl")    # particle container
+# include("core/io.jl")           # I/O
+# include("samplers/sampler.jl")  # samplers
+# include("core/ad.jl")           # Automatic Differentiation
 
 end
---
+++ src/core/composite_distribution.jl
@@ -0,0 +1,76 @@
+"""
+    _model(def::Expr)
+
+Internals for @model
+"""
+function _model(def::Expr)
+
+    # Split up the definition using MacroTools.
+    def_dict = splitdef(def)
+
+    # Explicitly disallow some things that we don't currently handle.
+    length(def_dict[:kwargs]) != 0 && throw(error("Don't support kwargs at the minute."))
+
+    # THIS IMPLEMENTATION IS SHIT. NEEDS TO BE IMPROVED. Currently allows fields of a
+    # particular type to be abstract, thus screwing over performance. Easy to fix though.
+    # Generate new type for the function based on its args.
+    type_signature = :($(def_dict[:name]){$(def_dict[:whereparams]...)} <: Distribution)
+    type_fields = Expr(:block, def_dict[:args]...)
+    model_type = Expr(:type, false, type_signature, type_fields)
+
+    # Generate logpdf by traversing body and playing around with the ~ - terms.
+    
+
+    return model_type
+end
+
+"""
+    model(def::Expr)
+
+A model.
+"""
+macro model(def::Expr)
+    return esc(_model(def))
+end
+
+# A possible Bayesian linear regressor.
+@model function foo(X::AbstractMatrix{T}, σw::T, σn::Real) where T<:Real
+    w ~ Normal(0, σw)
+    f = X * w
+    y ~ Normal(f, σn)
+end
+
+bar = :(function foo(X::AbstractMatrix{T}, σw::T, σn::Real) where T<:Real
+    w ~ Normal(0, σw)
+    f = X * w
+    y ~ Normal(f, σn)
+end)
+
+# Output should look something like this:
+struct MangledStruct{Tθ}
+    θ::Tθ
+end
+
+# Slow and ugly allocating implementation of rand. A good implementation would use rand!.
+function rand(rng::AbstractRNG, f::MangledStruct)
+    w = rand(rng, Normal(0, σw))
+    f = X * w
+    y = rand(rng, Normal(f, σn))
+    return vcat(w, y)
+end
+
+# Probably sensible logpdf evaluation function.
+function logpdf(foo::MangledStruct, x)
+    w = x[:w]
+    l = logpdf(Normal(0, foo.σw), w)
+    f = foo.X * w
+    y = x[:y]
+    l += logpdf(Normal(f, foo.σ), y)
+    return l
+end
+
+
+
+
+
+
